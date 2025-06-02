fdb9349382aa93e86c103e7c9e8d8495f9d710e8
Rolland_Dev <rolland.h.dev@gmail.com>
2025-06-02 20:00:10 +0200

    commit folio vue.js 2/6/25

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import Navbar from "../src/pages/Navbar.vue";
+</script>
+
+<template>
+  <Navbar />
+</template>
+
+<style scoped></style>
---
+++ src/components/Link.vue
@@ -0,0 +1,31 @@
+<script setup></script>
+
+<template>
+  <ul>
+    <li>Accueil</li>
+    <li>Skills</li>
+    <li>Folio</li>
+    <li>Contact</li>
+  </ul>
+</template>
+
+<style scoped>
+ul {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0 auto;
+}
+
+ul li {
+  list-style-type: none;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: black;
+  font-weight: 500;
+  font-size: 1.2rem;
+}
+</style>
