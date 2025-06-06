4062df3fd0cadfdd6d8614a0f155418343cd5cec
Rolland_Dev <rolland.h.dev@gmail.com>
2025-06-06 19:29:40 +0200

    commit 6/6/25 header close

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
+import Competences from "./pages/Competences.vue";
 import Header from "./pages/Header.vue";
 </script>
 
 <template>
   <Header />
+  <Competences />
 </template>
 
 <style scoped></style>
---
+++ src/pages/Competences.vue
@@ -0,0 +1,12 @@
+<script setup></script>
+
+<template>
+  <h2>Compétences</h2>
+</template>
+
+<style scoped>
+h2 {
+  text-align: center;
+  margin: 40px auto;
+}
+</style>
